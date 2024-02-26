2a47a27de568ed18cb8e262248e42dea5b41ca13
ZachShirikjian <zshirikjian@gmail.com>
2024-02-26 12:26:40 -0500

    ON Logo added to title and splash screen
    -Deleted all copyrighted music used
    -Added CC's video script
    -Added Owls Nest Logo to title screen and splash screen
    -Replaced in-game dialogue with new ones from the cutscene scripts
    -Videos now load into next scene automatically

--- Trillenium/Assets/Scripts/SplashScreen.cs
+++ Trillenium/Assets/Scripts/SplashScreen.cs
@@ -6,13 +6,21 @@ using UnityEngine.SceneManagement;
 //LOAD THE TITLE SCREEN AFTER THE SPLASH SCREEN PLAYS (CHANGE VALUE LATER IF MORE LOGOS ARE ADDED!)
 public class SplashScreen : MonoBehaviour
 {
+    public GameObject OwlsNestLogo;
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("LoadTitleScreen", 6f);
+        OwlsNestLogo.SetActive(false);
+        Invoke("LoadLogo", 5f);
     }
 
-    //Loads Title Screen after the ZS logo and sting plays
+    //Popup ON logo (& add others if needed later) then loads title screen after splash screen ends
+    void LoadLogo()
+    {
+        OwlsNestLogo.SetActive(true);
+        Invoke("LoadTitleScreen", 3f); //increase this if more logos are added
+    }
+
     void LoadTitleScreen()
     {
         SceneManager.LoadScene("TitleScreen");
---
+++ Trillenium/Assets/Scripts/VideoController.cs
@@ -0,0 +1,122 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class VideoController : MonoBehaviour
+{
+    #region Variables
+    public GameObject mainCamera; // Main camera. YOU HAVE TO MANUALLY SET THIS.
+    public VideoPlayer videoPlayer; // Video player attached to camera. Script should automatically set this.
+    public VideoPlayer player; // To access pausing capabilities. Script should automatically set this.
+    int videoState; // Controls whether or not the game is paused (0 = paused, 1 = unpaused) and is controlled with the space bar;
+    public bool gamePaused; // Just for personal extra readability;
+    bool end; //Boolean used to disable pausing when end is reached (ultimately just to avoid any potential bugs);
+
+    public InputActionAsset controls;
+    public InputActionReference pauseCutscene;
+    public InputActionReference skipCutscene;
+    #endregion
+
+    void Awake()
+    {
+        videoPlayer = mainCamera.GetComponent<VideoPlayer>();
+        // Play on awake defaults to true. Set it to false to avoid the url set
+        // below to auto-start playback since we're in Start().
+        videoPlayer.playOnAwake = false;
+
+        // By default, VideoPlayers added to a camera will use the far plane.
+        // Let's target the near plane instead.
+
+        //Uncommenting this disables video play
+        //videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.CameraNearPlane;
+
+        // This will cause our Scene to be completely opaque.
+        videoPlayer.targetCameraAlpha = 1;
+
+        // Video will NOT loop.
+        videoPlayer.isLooping = false;
+
+        // For detecting when the video has reached the end.
+        videoPlayer.loopPointReached += EndReached;
+
+        //Enables New Input System inputs for pausing and skipping cutscenes 
+        OnEnable();
+    }
+
+    void Start()
+    {
+        // The video is playing.
+        videoState = 1;
+
+        // Is the game paused? Different from the video itself being over.
+        // Since the scene is starting, the game should NOT be paused yet.
+        gamePaused = false;
+
+        end = false;
+
+        player = mainCamera.GetComponent<VideoPlayer>();
+
+        // Loads video into video player and plays it.
+       // videoPlayer.url = "ENTER VIDEO URL HERE";
+        videoPlayer.Play();
+    }
+
+    //Enable input for pausing or skipping the cutscene while it's playing.
+    private void OnEnable()
+    {
+        pauseCutscene.action.performed += VideoPause;
+        pauseCutscene.action.Enable();
+    }
+
+    private void OnDisable()
+    {
+        pauseCutscene.action.performed -= VideoPause;
+        pauseCutscene.action.Disable();
+    }
+
+    void Update()
+    {
+        // Controller for pausing the game.
+        // if (Input.GetKeyDown(KeyCode.Space) && end == false)
+        // {
+        //     VideoPause();
+        // }
+    }
+
+    //After a cutscene's done, load the next scene in the build index after 1 second.
+    void EndReached(UnityEngine.Video.VideoPlayer vp)
+    {
+        end = true;
+        // *****INSERT CODE FOR TRANSITIONING INTO NEXT SCENE*****
+        Invoke("LoadNextScene", 1f);
+    }
+
+    void VideoPause(InputAction.CallbackContext context)
+    {
+        if(end == false)
+        {
+                if (videoState > 0)
+                {
+                    player.Pause();
+                    gamePaused = true;
+                    // The video is paused.
+                    videoState = 0;
+                }
+                else if (gamePaused == true)
+                {
+                    player.Play();
+                    gamePaused = false;
+                    // The video is playing.
+                    videoState = 1;
+                }
+        }
+    }
+
+    void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
